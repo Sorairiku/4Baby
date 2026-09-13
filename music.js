f1f059c668bb05f74b1e993dc38b5419ac4d81bd
Kenyon Sison <152249068+Sorairiku@users.noreply.github.com>
2026-09-13 08:58:38 +0000

    Limit music to story and message3

--- music.js
+++ music.js
@@ -1,34 +1,17 @@
-(function () {
-    var STORAGE_KEY = "journeyMusic";
+ (function () {
     var journeyTrack = "songs/journey-placeholder.mp3";
     var messageTrack = "songs/message3-placeholder.mp3";
     var pageName = window.location.pathname.split("/").pop() || "index.html";
     var backgroundAudio;
     var volume = Number(localStorage.getItem("journeyVolume")) || 1;
 
-    function saveState(track, currentTime) {
-        localStorage.setItem(STORAGE_KEY, JSON.stringify({
-            track: track,
-            currentTime: currentTime
-        }));
-    }
-
-    function readState() {
-        try {
-            return JSON.parse(localStorage.getItem(STORAGE_KEY)) || null;
-        } catch (error) {
-            return null;
-        }
-    }
-
-    function stopJourneyMusic() {
+    function stopMusic() {
         if (backgroundAudio) {
             backgroundAudio.pause();
             backgroundAudio.removeAttribute("src");
             backgroundAudio.load();
             backgroundAudio = null;
         }
-        localStorage.removeItem(STORAGE_KEY);
     }
 
     function updateVolumeDisplay(volumeDisplay) {
@@ -36,26 +19,14 @@
         volumeDisplay.setAttribute("aria-label", "Volume " + Math.round(volume * 100) + "%");
     }
 
-    function playTrack(track, resume) {
-        var savedState = readState();
-        stopJourneyMusic();
+    function playTrack(track) {
+        stopMusic();
         backgroundAudio = new Audio(track);
         backgroundAudio.loop = true;
         backgroundAudio.preload = "auto";
         backgroundAudio.volume = volume;
         backgroundAudio.autoplay = true;
 
-        if (resume && savedState && savedState.track === track) {
-            backgroundAudio.currentTime = Number(savedState.currentTime) || 0;
-        }
-
-        backgroundAudio.addEventListener("timeupdate", function () {
-            saveState(track, backgroundAudio.currentTime);
-        });
-        backgroundAudio.addEventListener("error", function () {
-            saveState(track, backgroundAudio.currentTime || 0);
-        });
-        saveState(track, backgroundAudio.currentTime || 0);
         backgroundAudio.play().catch(function () {});
     }
 
@@ -76,9 +47,9 @@
         });
 
         playButton.addEventListener("click", function () {
-            playTrack(journeyTrack, true);
+            playTrack(journeyTrack);
         });
-        stopButton.addEventListener("click", stopJourneyMusic);
+        stopButton.addEventListener("click", stopMusic);
         volumeControl.addEventListener("input", function () {
             volume = Number(volumeControl.value);
             localStorage.setItem("journeyVolume", volume);
@@ -88,32 +59,11 @@
             updateVolumeDisplay(volumeDisplay);
         });
 
-        playTrack(journeyTrack, true);
-    }
-
-    function startJourney(event) {
-        event.preventDefault();
-        saveState(journeyTrack, 0);
-        playTrack(journeyTrack, false);
-        window.location.href = event.currentTarget.href;
-    }
-
-    if (pageName === "index.html") {
-        var startButton = document.querySelector(".welcome-button");
-        if (startButton) {
-            startButton.addEventListener("click", startJourney);
-        }
-        return;
-    }
-
-    if (pageName === "songs.html") {
-        stopJourneyMusic();
-        return;
+        playTrack(journeyTrack);
     }
 
     if (pageName === "message3.html") {
-        stopJourneyMusic();
-        playTrack(messageTrack, false);
+        playTrack(messageTrack);
         return;
     }
 
